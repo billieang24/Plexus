e20b7b74832f6da1b8de938676e64a5dd45b2e7d
Maron Justo <maron_justo@yahoo.com>
2013-06-11 15:17:06 +0800

    lokomoko

--- plexus/public/javascripts/plexusJS.js
+++ plexus/public/javascripts/plexusJS.js
@@ -9,23 +9,56 @@ var y = "#liTab"+num2;
 $(x).attr("class", "active");
 $(y).removeAttr("class");
 }
-function changeTextFriendRequest(x)
+function changeTextFriendRequest(x,y)
 {
-	if(x==0)
+	if(y==1)
 	{
-		document.getElementById('friendRequestButton').innerHTML="Friend Request Sent";
-		$('.friendRequestButton').dropdown();
+		document.getElementById('friendRequestButton').style.visibility="visible";
+			if(x==1)
+			{
+				document.getElementById('friendRequestButton').innerHTML="Add as Friend";
+				$('#friendRequestButton').attr("onClick", "addAsFriendButton()");
+				$('#unfriendCancelButton').attr("onClick", "addAsFriendButton(1)");
+			}
+			else if(x==2)
+			{
+				document.getElementById('friendRequestButton').innerHTML="Friend";
+				$('#friendRequestButton').attr("onClick", "friendButton()");
+				document.getElementById('unfriendCancelButton').innerHTML="Unfriend";
+				$('#unfriendCancelButton').attr("onClick", "friendButton(1)");
+				$('#friendRequestButton').dropdown();
+			}
 	}
-	else if(x==1)
-		document.getElementById('friendRequestButton').innerHTML="Add as friend";
+	else if(y==2)
+	{
+		document.getElementById('friendRequestButton').style.visibility="hidden";
+	}
+}
+function addAsFriendButton(x)
+{
+	document.getElementById('friendRequestButton').innerHTML="Friend Request Sent";
+	document.getElementById('unfriendCancelButton').innerHTML="Cancel Friend Request";
+	$('#friendRequestButton').dropdown();
+	if(x==1)
+		changeTextFriendRequest(1,1);
+}
+function friendButton(x)
+{
+	$('#friendRequestButton').dropdown();
+	if(x==1)
+		changeTextFriendRequest(1,1);
+}
+function textAreaAdjust(o) {
+    o.style.height = "47px";
+    o.style.height = (o.scrollHeight)+"px";
 }
 function hide (userName1,userName2)
 {
 	if (userId1!=userId2)
 	{
 		$(".hideEdit").hide();
-	};
-	else
+	}else{
 		$(".hideEdit").show();
+	}
 
 }
